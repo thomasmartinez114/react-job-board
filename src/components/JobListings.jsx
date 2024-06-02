20a4ce98e2f0ff8251496017b8ebc157f2eaf4d8
Thomas Martinez <tmartinez114@gmail.com>
2024-06-01 22:43:18 -0400

    show only 3 jobs

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -3,7 +3,7 @@ import jobs from "../jobs.json"
 import JobListing from "./JobListing"
 
 const JobListings = () => {
-  //   console.log(jobs)
+  const recentJobs = jobs.slice(0, 3) // pick 0-3 jobs
 
   return (
     <section className="bg-blue-50 px-4 py-10">
@@ -12,7 +12,7 @@ const JobListings = () => {
           Browse Jobs
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobs.map((job) => (
+          {recentJobs.map((job) => (
             <JobListing key={job.id} singleJob={job} />
           ))}
         </div>
