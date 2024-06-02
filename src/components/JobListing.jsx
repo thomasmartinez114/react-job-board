8f13cee8cff4f8596b3cc65601038a1a24cf384d
Thomas Martinez <tmartinez114@gmail.com>
2024-06-02 00:34:20 -0400

    More/Less buttons for card details

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,6 +1,15 @@
 import React from "react"
+import { useState } from "react"
 
 const JobListing = ({ singleJob }) => {
+  const [showFullDescription, setShowFullDescription] = useState(false)
+
+  let description = singleJob.description
+
+  if (!showFullDescription) {
+    description = description.substring(0, 90) + "..."
+  }
+
   return (
     <div className="bg-white rounded-xl shadow-md relative">
       <div className="p-4">
@@ -9,7 +18,14 @@ const JobListing = ({ singleJob }) => {
           <h3 className="text-xl font-bold">{singleJob.title}</h3>
         </div>
 
-        <div className="mb-5">{singleJob.description}</div>
+        <div className="mb-5">{description}</div>
+
+        <button
+          onClick={() => setShowFullDescription((prevState) => !prevState)}
+          className="text-indigo-500 mb-5 hover:text-indigo-600"
+        >
+          {showFullDescription ? "Less" : "More"}
+        </button>
 
         <h3 className="text-indigo-500 mb-2">{singleJob.salary} / Year</h3>
 
