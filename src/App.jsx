9ee5c29feb6c6ba8a4fa9ca945c0b19943effc5c
Thomas Martinez <tmartinez114@gmail.com>
2024-06-04 23:04:32 -0400

    installed react-router-dom and set HomePage as default index // migrated Hero to HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import React from "react"
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import HomeCards from "./components/HomeCards"
-import JobListings from "./components/JobListings"
-import ViewAllJobs from "./components/ViewAllJobs"
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom"
+import HomePage from "./pages/HomePage"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<HomePage />} />)
+)
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero title="Test Title" subtitle="This is the subtitle" />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-    </>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
