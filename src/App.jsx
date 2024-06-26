891b6a17eb0076f14633785f4d5e37a21de15db2
“devtommie” <devtommie114@gmail.com>
2024-06-25 23:37:46 -0400

    added JobsPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import React from "react"
+import React from 'react';
 import {
   Route,
   createBrowserRouter,
   createRoutesFromElements,
   RouterProvider,
-} from "react-router-dom"
-import MainLayout from "./layouts/MainLayout"
-import HomePage from "./pages/HomePage"
+} from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+import JobsPage from './pages/JobsPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
+    <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
     </Route>
   )
-)
+);
 
 const App = () => {
-  return <RouterProvider router={router} />
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
