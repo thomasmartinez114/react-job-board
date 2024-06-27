788d329a092fa215110935c7645b5b5915eafa6f
Thomas Martinez <devtommie114@gmail.com>
2024-06-27 00:46:26 -0400

    ternary rendering content

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,24 +1,24 @@
-import React from "react"
-import jobs from "../jobs.json"
-import JobListing from "./JobListing"
+import React from 'react';
+import jobs from '../jobs.json';
+import JobListing from './JobListing';
 
-const JobListings = () => {
-  const recentJobs = jobs.slice(0, 3) // pick 0-3 jobs
+const JobListings = ({ isHome = false }) => {
+  const jobListings = isHome ? jobs.slice(0, 3) : jobs; // pick 0-3 jobs
 
   return (
-    <section className="bg-blue-50 px-4 py-10">
-      <div className="container-xl lg:container m-auto">
-        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          Browse Jobs
+    <section className='bg-blue-50 px-4 py-10'>
+      <div className='container-xl lg:container m-auto'>
+        <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
+          {isHome ? 'Recent Jobs' : 'Browse Jobs'}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {recentJobs.map((job) => (
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+          {jobListings.map(job => (
             <JobListing key={job.id} singleJob={job} />
           ))}
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default JobListings
+export default JobListings;
