4ecd365209ac6b174c07008223c5976f3bcdaf06
Thomas Martinez <devtommie114@gmail.com>
2024-06-27 01:15:06 -0400

    installed react-spinners and added loading spinner

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import JobListing from './JobListing';
+import Spinner from './Spinner';
 
 const JobListings = ({ isHome = false }) => {
   const [jobs, setJobs] = useState([]);
@@ -26,11 +27,19 @@ const JobListings = ({ isHome = false }) => {
         <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
           {isHome ? 'Recent Jobs' : 'Browse Jobs'}
         </h2>
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-          {jobs.map(job => (
-            <JobListing key={job.id} singleJob={job} />
-          ))}
-        </div>
+        
+          {loading ? (
+            <Spinner loading={loading} />
+          ) : (
+            <>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+              {jobs.map(job => (
+                <JobListing key={job.id} singleJob={job} />
+              ))}
+              </div>
+            </>
+          )}
+        
       </div>
     </section>
   );
