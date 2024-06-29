32fc8286b18fa64dd6ae71d7c8ac781f25f3f312
Thomas Martinez <devtommie114@gmail.com>
2024-06-28 23:25:05 -0400

    form submission functional and logging to console

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -2,20 +2,41 @@ import { useState } from 'react';
 
 const AddJobPage = () => {
   const [title, setTitle] = useState('');
-  const [type, setType] = useState('');
+  const [type, setType] = useState('Full-Time');
   const [location, setLocation] = useState('');
   const [description, setDescription] = useState('');
-  const [salary, setSalary] = useState('');
+  const [salary, setSalary] = useState('Under $50K');
   const [companyName, setCompanyName] = useState('');
   const [companyDescription, setCompanyDescription] = useState('');
   const [contactEmail, setContactEmail] = useState('');
   const [contactPhone, setContactPhone] = useState('');
 
+  const submitForm = e => {
+    e.preventDefault();
+
+    // console.log(description);
+
+    const newJob = {
+      title,
+      type,
+      location,
+      description,
+      salary,
+      company: {
+        name: companyName,
+        description: companyDescription,
+        contactEmail,
+        contactPhone,
+      },
+    };
+    console.log(newJob);
+  };
+
   return (
     <section className='bg-indigo-50'>
       <div className='container m-auto max-w-2xl py-24'>
         <div className='bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0'>
-          <form>
+          <form onSubmit={submitForm}>
             <h2 className='text-3xl text-center font-semibold mb-6'>Add Job</h2>
 
             <div className='mb-4'>
