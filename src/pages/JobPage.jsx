cbe60ee6918c76f4d93beafa1b5cb9cfa3982598
Thomas Martinez <devtommie114@gmail.com>
2024-06-28 22:20:59 -0400

    used useLoaderData() instead of useEffect()

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,30 +1,38 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import Spinner from '../components/Spinner';
+// import { useState, useEffect } from 'react';
+import { useParams, useLoaderData } from 'react-router-dom';
+// import Spinner from '../components/Spinner';
 
 const JobPage = () => {
   const { id } = useParams();
-  const [job, setJob] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const job = useLoaderData();
+  //   const [job, setJob] = useState(null);
+  //   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchJob = async () => {
-      try {
-        const res = await fetch(`/api/jobs/${id}`);
-        const data = await res.json();
-        console.log(data);
-        setJob(data);
-      } catch (error) {
-        console.log('Error fetching data', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchJob();
-  }, []);
+  //   useEffect(() => {
+  //     const fetchJob = async () => {
+  //       try {
+  //         const res = await fetch(`/api/jobs/${id}`);
+  //         const data = await res.json();
+  //         console.log(data);
+  //         setJob(data);
+  //       } catch (error) {
+  //         console.log('Error fetching data', error);
+  //       } finally {
+  //         setLoading(false);
+  //       }
+  //     };
+  //     fetchJob();
+  //   }, []);
 
-  return loading ? <Spinner /> : <h1>{job.title}</h1>;
+  //   return loading ? <Spinner /> : <h1>{job.title}</h1>;
+  return <h1>{job.title}</h1>;
 };
 
-export default JobPage;
+const jobLoader = async ({ params }) => {
+  const res = await fetch(`/api/jobs/${params.id}`);
+  const data = await res.json();
+  return data;
+};
+
+export { JobPage as default, jobLoader };
